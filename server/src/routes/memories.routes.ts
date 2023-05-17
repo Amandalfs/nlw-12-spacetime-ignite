d904290459295bd83d4a5f8ab9ec0b2338592529
Amandalfs <caduc57@gmail.com>
2023-05-17 11:02:18 -0300

    feat
    criado a memorie

--- server/src/routes/memories.routes.ts
+++ server/src/routes/memories.routes.ts
@@ -35,7 +35,26 @@ export async function memoriesRoutes(app: FastifyInstance) {
     return memory
   })
 
-  app.post('/memories', async () => {})
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string().default('.'),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memorie = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: 'cd47bc9f-2e84-4dd8-84ad-d9569f74a084',
+      },
+    })
+
+    return memorie
+  })
 
   app.put('/memories', async () => {})
 
