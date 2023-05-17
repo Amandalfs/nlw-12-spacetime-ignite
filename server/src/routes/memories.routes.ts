4361d7da04af039f4229993f1dcd78447dd774a2
Amandalfs <caduc57@gmail.com>
2023-05-17 12:08:39 -0300

    feat
    atualizacao da memoria

--- server/src/routes/memories.routes.ts
+++ server/src/routes/memories.routes.ts
@@ -56,7 +56,35 @@ export async function memoriesRoutes(app: FastifyInstance) {
     return memorie
   })
 
-  app.put('/memories', async () => {})
+  app.put('/memories/:id', async () => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string().default('.'),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memorie = await prisma.memory.update({
+      where: {
+        id,
+      },
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: 'cd47bc9f-2e84-4dd8-84ad-d9569f74a084',
+      },
+    })
+
+    return memorie
+  })
 
   app.delete('/memories/:id', async (request) => {
     const paramsSchema = z.object({
