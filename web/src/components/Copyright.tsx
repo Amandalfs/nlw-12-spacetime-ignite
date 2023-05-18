01ee34bb3f21825acd9de7d11439954e1729d677
Amandalfs <caduc57@gmail.com>
2023-05-17 21:03:22 -0300

    feat
    componetizacao de alguns elementos

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,14 @@
+export default function Copyright() {
+  return (
+    <div className="text-sn leading-relaxed text-gray-500">
+      Feito com 💜 no NLW da{' '}
+      <a
+        className="underline hover:text-gray-100"
+        rel="norefeffer"
+        href="https://rocketseat.com.br"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
